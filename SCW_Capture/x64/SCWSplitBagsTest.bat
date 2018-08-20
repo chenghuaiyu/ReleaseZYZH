14a3c3a86db064d87c7ac13e85675486faf0c1dd
chenghuaiyu@gmail.com <chenghuaiyu@gmail.com>
2018-08-20 17:41:41 +0800

    SCW_Capture

---
+++ SCW_Capture/x64/SCWSplitBagsTest.bat
@@ -0,0 +1,18 @@
+:: SCWSplitBagsTest.exe "C:\images\captures\2018-02-03\config.ini" "C:\images\captures\2018-02-03\haerbin_%%04d.jpg" "..\bags"
+
+:: SCWSplitBagsTest.exe "C:\images\captures\2018-02-03\config.ini" "C:\images\captures\2018-02-04\haerbin_%%04d.jpg" "..\bags"
+
+:: SCWSplitBagsTest.exe "C:\images\captures\2018-02-03\config.ini" "C:\images\captures\2018-02-05\haerbin_%%04d.jpg" "..\bags"
+
+SCWSplitBagsTest.exe "C:\images\captures\2018-02-03\config.ini" "C:\images\captures\2018-02-05_2\haerbin_%%04d.jpg" "..\bags"
+
+
+
+
+
+
+
+
+
+
+
